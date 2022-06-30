bb1b715e1cc35dd1365c81c48e596293c28bed2f
Vision-Tey <nimusiimavision@gmail.com>
2022-06-30 09:48:52 +0300

    Test drag effect

--- editor-app/src/components/leftMenu/LeftMenu.js
+++ editor-app/src/components/leftMenu/LeftMenu.js
@@ -1,69 +1,60 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import "./LeftMenu.css";
 import { editSecOptions } from "../../data/data";
 
 
-import Align from "./comp/Align";
-import TextFill from "./comp/TextFill";
-import Image from "./comp/Image";
-import Table from "./comp/Table";
-import Signs from "./comp/Signs";
-import Calender from "./comp/Calender";
-import DropDown from "./comp/DropDown";
-import TextBox from './comp/TextBox';
+// import Align from "./comp/Align";
+// import TextFill from "./comp/TextFill";
+// import Image from "./comp/Image";
+// import Table from "./comp/Table";
+// import Signs from "./comp/Signs";
+// import Calender from "./comp/Calender";
+// import DropDown from "./comp/DropDown";
+// import TextBox from './comp/TextBox';
 
 import MidSection from '../midSection/MidSection';
+import { TextBox } from '../midSection/MidSection';
 
 
 
+const NavButton = ({ customFunc, icon }) => (
+  <button type='button'
+    draggable='true'
+    onDragStart={dragStart}
+  >
+    <img src={icon} alt="icon" />
+  </button>
+)
 
-const initialState = {
-  align: false,
-  textfill: false,
-  image: false,
-  table: false,
-  signs: false,
-  calendar: false,
-  dropdown: false,
+const dragStart = e => {
+  e.dataTransfer.setData("text/plain", "TEXT_INPUT");
+  console.log("drag start");
 }
 
-
- const NavButton = ({ customFunc, icon }) => (
-      <button type='button' 
-        draggable='true'
-        onDragStart = {dragStart}
-      >
-        <img src={icon} alt="icon" />
-      </button>
-  )
- const dragStart =  e => {
-
-    e.dataTransfer.setData("text/plain", "text");
-
- }
-
-  
-const LeftMenu = ({ showSidebar }) => {
-  const onDrop = (event) => {
-    event.preventDefault();
+export const onDrop = (event) => {
+  if (event.dataTransfer.getData("text/plain") === "TEXT_INPUT") {
     const data = event.dataTransfer.getData("text/plain");
     console.log(data);
+    const textBox = document.createElement("div");
+    textBox.className = "TEXT_INPUT";
+    textBox.innerHTML = data;
+    event.target.appendChild(textBox);
+
   }
+}
 
-  const dragOver = (event) => {
-    const isLink = event.dataTransfer.types.includes("text/plain");
-    if (isLink) {
-      event.preventDefault();
-    }
+export const dragOver = (event) => {
+  const isLink = event.dataTransfer.types.includes("text/plain");
+  if (isLink) {
+    event.preventDefault();
+    console.log("drag over");
   }
-
- const [isClicked, setIsClicked] = useState(initialState);
-
-<MidSection style={{display:"none"}} onDragOver ={dragOver} onDrop={onDrop}/>
+}
+const LeftMenu = ({ showSidebar }) => {
 
   return (
     <div className="leftMenu">
-      
+
       <NavButton icon={editSecOptions[0].icon} />
       <NavButton icon={editSecOptions[1].icon} />
       <NavButton icon={editSecOptions[2].icon} />
@@ -72,15 +63,6 @@ const LeftMenu = ({ showSidebar }) => {
       <NavButton icon={editSecOptions[5].icon} />
       <NavButton icon={editSecOptions[6].icon} />
 
-      
-
-      {isClicked.align && <Align />}
-        {isClicked.textfill && <TextFill />}
-        {isClicked.image && <Image />}
-        {isClicked.table && <Table />}
-        {isClicked.signs && <Signs />}
-        {isClicked.calendar && <Calender />}
-        {isClicked.dropdown && <DropDown />}
 
     </div>
   );
--- editor-app/src/components/midSection/MidSection.js
+++ editor-app/src/components/midSection/MidSection.js
@@ -2,16 +2,27 @@ import React from "react";
 import { Container } from "react-bootstrap";
 import "./MidSection.css";
 
+import { onDrop, dragOver } from "../leftMenu/LeftMenu";
 
-const MidSection = ({onDragOver, onDrop}) => {
+export const TextBox = () => {
+  return (
+    <div className="textBox">
+     <div>passing data</div> 
+    </div>
+  );
+}
+const MidSection = () => {
 
   return (
+  
+    
     <div className="midSection">
-      <Container as="div" className="midSection_container" onDragOver={onDragOver}
+      <Container as="div" className="midSection_container" onDragOver={dragOver}
         onDrop={onDrop}>
        
       </Container>
     </div>
+  
   );
 };
 
