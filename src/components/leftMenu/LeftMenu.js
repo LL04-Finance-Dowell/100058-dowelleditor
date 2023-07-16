53cbf2cd01fb5a9623af8e664baeff5cf82ca0ab
Mubeen Mughal <mubeenmughal111@gmail.com>
2023-07-16 02:28:31 -0700

    Fix left menu resposiveness on mobile view

--- src/components/leftMenu/LeftMenu.js
+++ src/components/leftMenu/LeftMenu.js
@@ -346,7 +346,7 @@ const LeftMenu = ({ showSidebar }) => {
               icon={<FaCamera />}
               title="camera"
             />
-            }: ""
+            }
            {
            //"}"
           }
